ec6127a1c27ad03898544358833cdb7566d8d833
DrashtiGabani <drashtigabani48@gmail.com>
2024-12-06 11:01:28 +0100

    error_gone

--- python_1.py
+++ python_1.py
@@ -1,5 +1,4 @@
-print("Hello")
+print("Hello world")
 print("Edited from github website")
 print("one more edition")
-print("third change from pycharm")
-print("This line is edited by third person from outside")
+
